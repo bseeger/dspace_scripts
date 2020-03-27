2f4fec22bc79bf9e6f1a2e5b537124b5926c14bb
bseeger <bseeger@jhu.edu>
2020-03-27 09:36:57 -0400

    Cleaned up a little and now gets the ids for two ingest in jhu_ingest_csv

--- fetchItems.py
+++ fetchItems.py
@@ -12,15 +12,31 @@ from os import path
 # author: bseeger@jhu.edu
 # since: March 2020
 
-item_uuids = [
-    "3285513f-ea01-458f-a089-446e2493f255",
-    "7ba72efa-0b7c-409c-9041-2c03a31e0d1a",
-    # more UUIDs here
-]
+from ingest_1_uuid import item_uuids as uuids_1
+from ingest_2_uuid import item_uuids as uuids_2
+#
+item_uuids = uuids_1 + uuids_2
+
+# or
+#item_uuids = [
+#   "3285513f-ea01-458f-a089-446e2493f255",
+#   "7ba72efa-0b7c-409c-9041-2c03a31e0d1a",
+#   "58f1ec2e-e681-4c5d-8c2d-6d9def4177bf",
+#   "f3a750f0-45c6-40a4-9d94-36451f4b3bce",
+#   "cebd28c8-b92e-478f-aa4c-c3047865f121"
+#   ]
 
 verbose = False
-data_dir = "./images"
+data_dir = "./binaries"
+drupal_data_dir = "/var/www/html/drupal/web/modules/contrib/jhu_ingest_csv/data/files"
 rest_url = "https://jscholarship.library.jhu.edu/rest/"
+csv_schema = ["ID", "parent_id", "collection", "uuid", "date_accessioned", "date_available", "date issued",
+              "identifier_other", "identifier_uri", "description", "language",
+              "publisher", "relation", "rights", "subject", "title",
+              "type", "model", "media_use", "mimetype", "sequence_id", "display", "file"]
+
+def normalize_filename(filename):
+    return filename.replace(' ', '_').replace('[', '_').replace(']', '_')
 
 def handle_metadata(meta_dict, data, verbose):
     keys = meta_dict.keys() 
@@ -32,7 +48,7 @@ def handle_metadata(meta_dict, data, verbose):
                  "" if "dc.date.issued" not in keys else meta_dict["dc.date.issued"],
                  "" if "dc.identifier.other" not in keys else  meta_dict["dc.identifier.other"],
                  "" if "dc.identifier.uri" not in keys else meta_dict["dc.identifier.uri"],
-                 "" if "dc.description.abstract" not in keys else meta_dict["dc.description.abstract"],
+                 "" if "dc.description.abstract" not in keys else meta_dict["dc.description.abstract"].encode('utf-8'),
                  "" if "dc.language.iso" not in keys else meta_dict["dc.language.iso"],
                  "" if "dc.publisher" not in keys else meta_dict["dc.publisher"],
                  "" if "dc.relation" not in keys else meta_dict["dc.relation"],
@@ -43,67 +59,22 @@ def handle_metadata(meta_dict, data, verbose):
                 ])
 
 
-def handle_bitstream(bin_info, data, dir, rest_url, verbose, meta_dict):
+#def handle_bitstream(bin_info, data, dir, rest_url, verbose, meta_dict):
     # pull the retrieve link from json
     # download the file to 'dir' directory
     # note the name of the file in the csv file
-    i = 0 
-    for bs in bin_info:
-        print("FILES: looking at '{}'".format(bs['bundleName']))
-        # quick and dirty check
-        if i > 1:
-            print("there is more the one original file on uuid: {} ({})"
-                    .format(bs['uuid'], bs['description']))
 
-        if bs['bundleName'] == "ORIGINAL":
-            # hack. yuck. Only grab the audio for this mime type (for now, as
-            # there are transcriptions there as well to be considered later)
-            # I will fix this once I better know what my goals for this are. 
-            if (meta_dict['dc.format.mimetype'] == 'audio/mpeg' and 
-               (bs['description'] != 'audio' or bs['description'] != "audio 1")):
-                continue;
-
-            i += 1
-            url = rest_url + bs['retrieveLink'][6:] 
-            if verbose:
-                print("looking for original bit stream at: '{}'".format(url))
-
-            file_loc = path.join(dir, bs['name'])
-            if verbose:
-                print("Storing data in: '{}'".format(file_loc))
-
-            # get file via requests
-            resp = requests.get(url)
-            if resp.status_code != requests.codes.ok:
-                print("There was an issue retrieving the file ({}): '{}'".format(resp.status_code, url))
-                resp.raise_for_status()
-
-            # store file (may be one step)
-            with open(file_loc, 'wb') as fp:
-                fp.write(resp.content)
-
-            # this will not go well if there is more the one ORIGINAL file.  :-)  Time will tell...
-            data.extend([file_loc])
-
+   
 
 
 if __name__ == '__main__':
 
-#    with open("config.yml", 'r') as ymlfile:
-#        cfg = yaml.load(ymlfile)
-
-    schema = ["ID", "uuid", "date_accessioned", "date_available", "date issued",
-              "identifier_other", "identifier_uri", "abstract", "language",
-              "publisher", "relation", "rights", "subject", "title",
-              "type"]
-
     id = 0
-
 
     with open('./JHUTestIngest.csv', 'w') as csvfile:
 
         writeCSV = csv.writer(csvfile)
-        writeCSV.writerow([r for r in schema])
+        writeCSV.writerow([r for r in csv_schema])
 
 
         for uuid in item_uuids:
@@ -131,7 +102,9 @@ if __name__ == '__main__':
             if verbose:
                 print(bininfo)
 
-            data = [id, uuid]
+            parent_id = ""  # TODO fix me, place holder for now
+            collection = "" # TODO fix me, place holder for now
+            data = [id, parent_id, collection, uuid]
 
             # Parse the metadata into approprate information to put into the csv file. 
 
@@ -140,10 +113,40 @@ if __name__ == '__main__':
                 meta_dict[i['key']] = i['value']
 
             handle_metadata(meta_dict, data, verbose)
-            handle_bitstream(bininfo, data, data_dir, rest_url, verbose, meta_dict)
 
-            if verbose:
-                print(data)
-            writeCSV.writerow(data)
+            # one row for every binary just to see what it looks like
+            for bs in bininfo:
+                url = rest_url + bs['retrieveLink'][6:] 
+                
+                filename = normalize_filename(bs['name'])
+                file_loc = path.join(data_dir, filename)
+                if verbose:
+                    print("Storing binary in: '{}'".format(file_loc))
+
+                # save everything right now, but if you want to exclude some, put the 
+                # mimetypes in the array
+                if bs['mimeType'] not in ["put types here to exclude"]:
+                    # get file via requests
+                    resp = requests.get(url)
+                    if resp.status_code != requests.codes.ok:
+                        print("There was an issue retrieving the file ({}): '{}'".format(resp.status_code, url))
+                        resp.raise_for_status()
+                 
+                    # store file (may be one step)
+                    with open(file_loc, 'wb') as fp:
+                        fp.write(resp.content)
+                else:
+                    print ("Skipping {}".format(bs['mimeType']))
+
+                drupal_file_loc = path.join(drupal_data_dir, filename)
+
+                # TODO:
+                # 1) add logic to figure out display hint based on mimetype
+                # 2) add logic to figure out model 
+                # 3) If item has more then one page, insert a Paged-Content object with all metadata first. 
+                if verbose:
+                    print(data + [bs['bundleName'], bs['mimeType'], bs['sequenceId'],file_loc])
+                writeCSV.writerow(data + ["MODEL", bs['bundleName'], bs['mimeType'], bs['sequenceId'], "DISPLAY", drupal_file_loc])
+
 
     print("Done!")
